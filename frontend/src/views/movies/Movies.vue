9217a0a5e97436aacb0ab3ef1f40f469ab5491df
Joe-PC\Joe <joe790924@gmail.com>
2019-04-23 01:08:37 +0800

    movies page selectors / custom scrollbar

--- frontend/src/views/movies/Movies.vue
+++ frontend/src/views/movies/Movies.vue
@@ -11,7 +11,17 @@
 					<input type="text" placeholder="Find Movies..." class="movie-input">
 				</el-col>
 				<el-col :xs="24" :sm="6">
-					<div class="selectors"></div>
+					<div class="selectors">
+						<span class="selected" @click="toggleSelect = true">{{category}}</span>
+						<div class="options-box" v-show="toggleSelect">
+							<div class="option" @click="switchCategory('All Categories')">All Categories</div>
+							<div class="option" @click="switchCategory('Action')">Action</div>
+							<div class="option" @click="switchCategory('Comedy')">Comedy</div>
+							<div class="option" @click="switchCategory('Love')">Love</div>
+							<div class="option" @click="switchCategory('Drama')">Drama</div>
+						</div>
+						<div class="option-mask" v-if="toggleSelect" @click="toggleSelect = false"></div>
+					</div>
 				</el-col>
 				<el-col :xs="24" :sm="8">
 					<div class="search-btn">SEARCH</div>
@@ -26,11 +36,23 @@ import { getMovies } from '@/api/movie'
 
 export default {
 	name: 'Movies',
+	data() {
+		return {
+			toggleSelect: false,
+			category: 'All Categories'
+		}
+	},
 	mounted() {
 		getMovies().then(res => {
 			console.log(res);
 		});
 	},
+	methods: {
+		switchCategory(type) {
+			this.category = type;
+			this.toggleSelect = false;
+		}
+	}
 }
 </script>
 
@@ -95,6 +117,46 @@ export default {
 				height: 100%;
 				background-color: #e7e7e7;
 				border-radius: 0 8px 8px 0;
+				position: relative;
+				.selected {
+					position: relative;
+					z-index: 1;
+					height: 100%;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					cursor: pointer;
+				}
+				.options-box {
+					width: 100%;
+					position: absolute;
+					z-index: 1;
+					left: 0;
+					bottom: -108px;
+					background-color: #f8f9fa;
+					box-sizing: border-box;
+					padding: 4px;
+					height: 108px;
+					overflow-y: auto;
+					text-align: left;
+					.option {
+						height: 25px;
+						line-height: 25px;
+						cursor: pointer;
+						&:hover {
+							background-color: $rd1;
+							color: #fff;
+						}
+					}
+				}
+				.option-mask {
+					position: fixed;
+					top: 0;
+					left: 0;
+					width: 100vw;
+					height: 100vh;
+					z-index: 0;
+				}
 			}
 			.search-btn {
 				color: #fff;
